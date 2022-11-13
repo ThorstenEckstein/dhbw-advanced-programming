872fa1e9d14fed240c9cfbb5e3f9593949d9802b
thorsteneckstein <thorsten_eckstein@web.de>
2022-11-13 19:43:29 +0100

    added docs as IntelliJ module

--- course-1/src/de/dhbw/ss23/Course1Main.java
+++ course-1/src/de/dhbw/ss23/Course1Main.java
@@ -1,3 +1,5 @@
+package de.dhbw.ss23;
+
 public class Course1Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ docs/src/de/dhbw/ss23/DocsMain.java
@@ -0,0 +1,7 @@
+package de.dhbw.ss23;
+
+public class DocsMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

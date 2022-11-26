7dd9985420f70d7012c8a77d0a02167de6c64588
thorsteneckstein <thorsten_eckstein@web.de>
2022-11-26 17:01:31 +0100

    clean up

--- course-12/test/de/dhbw/course12/Assertions.java
+++ course-12/test/de/dhbw/course12/Assertions.java
@@ -2,31 +2,36 @@ package de.dhbw.course12;
 
 public class Assertions {
 
+    public static final String ASSERTION_FAILURE =
+            "[Assertion Failure] Expected '%s', but was '%s'";
+    public static final String ASSERTION_FAILURE_WITH_DEFAULT =
+            "[Assertion Failure] Expected '%s', but was '%s' (default='%s')";
+
     public static void assertEquals(String expected, String actual) {
         if (!expected.equals(actual)) {
-            String message = String.format("[Assertion Failure] Expected '%s', but was '%s'", expected, actual);
+            String message = String.format(ASSERTION_FAILURE, expected, actual);
             System.err.println(message);
         }
     }
 
-    public static void assertEquals(String expected, String actual, String actualDefault) {
+    public static void assertEquals(String expected, String actual, String _default) {
         if (!expected.equals(actual)) {
             String message = String.format(
-                    "[Assertion Failure] Expected '%s', but was '%s' (default='%s')", expected, actual, actualDefault);
+                    ASSERTION_FAILURE_WITH_DEFAULT, expected, actual, _default);
             System.err.println(message);
         }
     }
 
     public static void assertEquals(int expected, int actual) {
         if (expected != actual) {
-            String message = String.format("[Assertion Failure] Expected '%s', but was '%s'", expected, actual);
+            String message = String.format(ASSERTION_FAILURE, expected, actual);
             System.err.println(message);
         }
     }
 
     public static void assertTrue(boolean actual) {
-        if (actual == false) {
-            String message = String.format("[Assertion Failure] Expected '%s', but was '%s'", true, actual);
+        if (!actual) {
+            String message = String.format(ASSERTION_FAILURE, true, actual);
             System.err.println(message);
         }
     }

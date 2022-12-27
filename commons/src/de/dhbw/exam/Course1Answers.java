1fb9503934a071d7b31deb302417921c428e82c3
thorsteneckstein <thorsten_eckstein@web.de>
2022-12-27 11:09:58 +0100

    Course 1: added exam tests

--- commons/src/de/dhbw/exam/Course1Answers.java
+++ commons/src/de/dhbw/exam/Course1Answers.java
@@ -3,7 +3,8 @@ package de.dhbw.exam;
 public class Course1Answers extends Answers {
 
     static {
-        //answers.put(Q1, "<answer-for-Q1>");
+        answers.put(Q1, "D");
+        answers.put(Q2, "A");
     }
 
 }
--- course-1/test/de/dhbw/course1/exam/ExamTests.java
+++ course-1/test/de/dhbw/course1/exam/ExamTests.java
@@ -17,7 +17,28 @@ public class ExamTests {
 
     @DisplayName(
         """
-        Q1: not yet specified
+        Q1: Consider the following application:
+        
+        class MyApp {
+          public static void main(String[] args) {
+            Holder h = new Holder();
+            h.held = 100;
+            h.bump(h);
+            System.out.println(h.held);
+          }
+        }
+        class Holder {
+          public int held;
+          public void bump(Holder h) {
+            h.held++;
+          }
+        }
+        
+        What will be the output?
+        A. 0
+        B. 1
+        C. 100
+        D. 101
         """
     )
     // Please give your answer here:
@@ -36,11 +57,14 @@ public class ExamTests {
 
     @DisplayName(
             """
-            Q2: not yet specified
+            Q2: Which of the following will print "true" to the console?
+            
+            A. int x = 100; float y = 100.0F; System.out.println(x == y);
+            B. String x = new String("100"); String y = new String("100");System.out.println(x == y);
             """
     )
     // Please give your answer here:
-    @ValueSource(strings = {"?"})
+    @ValueSource(strings = {"A"})
     @ParameterizedTest
     public void question2(String studentAnswer) {
         // given

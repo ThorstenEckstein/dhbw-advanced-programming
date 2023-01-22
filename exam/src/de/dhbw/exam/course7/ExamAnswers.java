3161f6a39d53981f649eab5e5f1e5b76a4fbd739
thorsteneckstein <thorsten_eckstein@web.de>
2023-01-22 14:01:20 +0100

    course-8-10: added exam test classes, but no questions yet

---
+++ exam/src/de/dhbw/exam/course7/ExamAnswers.java
@@ -0,0 +1,11 @@
+package de.dhbw.exam.course7;
+
+import de.dhbw.exam.Answers;
+
+public class ExamAnswers extends Answers {
+
+    static {
+        // No answers yet
+    }
+
+}
---
+++ exam/src/de/dhbw/exam/course9/ExamAnswers.java
@@ -0,0 +1,12 @@
+package de.dhbw.exam.course9;
+
+import de.dhbw.exam.Answer;
+import de.dhbw.exam.Answers;
+
+public class ExamAnswers extends Answers {
+
+    static {
+        answers.put(Q1, Answer.from("A")); // DUMMY
+    }
+
+}

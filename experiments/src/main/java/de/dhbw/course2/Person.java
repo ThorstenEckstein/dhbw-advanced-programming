d42c93eb7717c7d962fb71b26b2663c5456aaa48
thorsteneckstein <thorsten_eckstein@web.de>
2023-02-17 15:17:15 +0100

    course 2: exercise updates, new pdf's (solutions in /exam)

---
+++ experiments/src/main/java/de/dhbw/course2/Person.java
@@ -0,0 +1,17 @@
+package de.dhbw.course2;
+
+/*
+ * EXAMPLE CLASS FOR IDE USAGE:
+ *  - Code-Generation
+ *  - Auto-Completion
+ *  - Refactoring-Proposals
+ *
+ * Steps:
+ *  - create name attribute manually
+ *  - use short-cut 'control + enter' to generate methods and constructor
+ *  - use menu 'refactoring' to introduce the interface 'HumanBeing'
+ */
+
+public class Person {
+
+}

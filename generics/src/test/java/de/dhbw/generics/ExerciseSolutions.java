d7773077bc40480d066cc52eac1b3f8de44794bd
thorsteneckstein <thorsten_eckstein@web.de>
2023-03-06 10:45:15 +0100

    generics: added second exercise

--- generics/src/test/java/de/dhbw/generics/ExerciseSolutions.java
+++ generics/src/test/java/de/dhbw/generics/ExerciseSolutions.java
@@ -26,4 +26,17 @@ public class ExerciseSolutions {
         // then
     }
 
+    @Test
+    @DisplayName("Exercise 2")
+    public void exercise2Solution() {
+        /*
+        public interface Calculator<T extends Number> {
+            T add(T first, T second);
+            T remove(T first, T second);
+            T multiply(T first, T second);
+            T divide(T first, T second);
+        }
+        */
+    }
+
 }

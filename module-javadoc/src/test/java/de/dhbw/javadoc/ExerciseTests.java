c4ea837d0d97ef0187154b9027b97c223d5b83ca
thorsteneckstein <thorsten_eckstein@web.de>
2023-03-13 14:11:14 +0100

    modules: new 'exceptions' and 'javadoc'

---
+++ module-javadoc/src/test/java/de/dhbw/javadoc/ExerciseTests.java
@@ -0,0 +1,40 @@
+package de.dhbw.javadoc;
+
+import de.dhbw.commons.Logger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExerciseTests {
+
+    private final Logger logger = new Logger(ExerciseTests.class);
+    @Test
+    @DisplayName("Exercise 1: ...")
+    public void exercise1() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("Exercise 2: ...")
+    public void exercise2() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("Exercise 3: ...")
+    public void exercise3() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
